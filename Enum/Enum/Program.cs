29f21139ac6e14800b1c478df0fa12f87c599e30
Olgerd425 <78820918+Olgerd425@users.noreply.github.com>
2021-02-25 12:29:34 +0200

    Add files via upload

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        enum Muscle 
+        { 
+        Biceps=1,
+        Pecs=2,
+        Calves=3,
+        Core=4,
+        Forearms=5
+        }
+        static void Main(string[] args)
+        {
+            Muscle complex;
+            complex = Muscle.Biceps;
+            Console.Write(complex);
+        }
+    }
+}
