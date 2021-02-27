a5ac7066c8ce064e8ad72e85744f82bbfe8b9ce4
Olgerd425 <78820918+Olgerd425@users.noreply.github.com>
2021-02-27 16:04:25 +0200

    Add files via upload

---
+++ ClassinMethod/ClassinMethod/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassinMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class user = new Class();
+            Method1(ref user);
+            Discord a = new Discord("Olgerd");
+            Method2(a);
+        }
+        static void Method1(ref Class user) 
+        {
+            Console.WriteLine($"Имя:{user.name} || Фамилия:{user.surname} || Возраст:{user.age}");
+        }
+
+        static void Method2(Discord a) 
+        {
+            Console.WriteLine(a.nickname, a.ID, a.server);
+        }
+
+    }
+    class Class
+    {
+        public int age = 16;
+        public string name = "Олег";
+        public string surname = "неизвестно";
+    }
+    class Discord 
+    {
+        public string nickname;
+        public int ID = 1234;
+        public string server = "DiscordServer";
+        public void Nickname(string nickname) 
+        {
+            this.nickname = nickname;
+        }
+    }
+}
